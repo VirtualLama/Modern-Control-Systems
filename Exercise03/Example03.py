c5197c9015519d456deeae0cc70345a66b4a0bc6
VirtualLama <paul.funk@rwth-aachen.de>
2021-08-03 13:14:39 +0200

    created subfolder

--- Exercise03/Example03.py
+++ Exercise03/Example03.py
@@ -1,5 +1,5 @@
 import control as cnt
-from numpy import reshape, shape, linalg
+from numpy import reshape, linalg
 i = complex(0, 1)
 
 # defining sytem matrices
@@ -17,12 +17,9 @@ else:
 
 # poles where given
 p = [-2+4*i, -2-4*i, -10]
-
 K = cnt.place(A, B, p)
 
 # output to check if the poles are correct
 print('controller: ', K)
 A = [0, 0, 1, 0, 1, -6, 1, -6, 31]
 A = reshape(A, (3, 3))
-print(shape(A))
-print(A)
