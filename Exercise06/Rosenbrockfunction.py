f4b4176bef782318359309fa86ef91dacf85450f
VirtualLama <paul.funk@rwth-aachen.de>
2021-08-05 18:35:56 +0200

    added TODO

--- Exercise06/Rosenbrockfunction.py
+++ Exercise06/Rosenbrockfunction.py
@@ -2,16 +2,27 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib
 import numpy as np
+
 # open in seperat window
 matplotlib.use('tkagg')
+
+# rosenbrock coefficients from wikipedia
 a = 1
 b = 100
+
+# set up a meshgrid
 x = y = np.arange(0, 10, 0.01)
 X, Y = np.meshgrid(x, y)
+
+# Rosenbrock function values
 Z = (a-X**2)**2 + b*(Y-X)**2
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(X, Y, Z)
+ax.plot_surface(X, Y, Z, )
+
+# TODO: add newton method and steepest gradient method
+# by adding each solution to the meshgrid
+
+# to show and keep the plots in place before exiting
 plt.show()
 input('Press enter to exit')
-print(x)
