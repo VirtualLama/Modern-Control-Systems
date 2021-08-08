a274d03e673031c5072cafb3a6a6d2a8623c6094
VirtualLama <paul.funk@rwth-aachen.de>
2021-08-08 22:45:21 +0200

    colord rosenbrock function plot

--- Exercise06/Rosenbrockfunction.py
+++ Exercise06/Rosenbrockfunction.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib
+from matplotlib import cm
 import numpy as np
 
 # open in seperat window
@@ -11,18 +12,18 @@ a = 1
 b = 100
 
 # set up a meshgrid
-x = y = np.arange(0, 10, 0.01)
+x = y = np.arange(0, 6, 0.001)
 X, Y = np.meshgrid(x, y)
 
 # Rosenbrock function values
 Z = (a-X**2)**2 + b*(Y-X)**2
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(X, Y, Z, )
+ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, vmin = 0, vmax=1000)
 
 # TODO: add newton method and steepest gradient method
 # by adding each solution to the meshgrid
 
 # to show and keep the plots in place before exiting
-plt.show()
-input('Press enter to exit')
+plt.show(block='true')
+#input('Press enter to exit')
